92f216babe2f86c2b0db1e57bbd20952c8f07b4b
jessicachacaaa <njessadega@gmail.com>
2014-10-04 15:41:23 +0700

    versi 2.3.2

---
+++ src/id/crowns/tugassatu/Jessicaa.java
@@ -0,0 +1,22 @@
+
+package id.crowns.tugassatu;
+
+
+public class Jessica {
+
+    public static void main(String[] args) {
+        //untuk menampilkan nama ke dalam variabel
+        String bio = "BIODATA";
+        String nama = "Jessica";
+        String kelas = "SI 13 E";
+        String npm = "13311763";
+        String ps = "S1 Sistem Informasi";
+        String ttl = "Bandarlampung, 27 Januari 1996";
+        String as = "SMK Utama Bandarlampung";
+        String ak = "2013";
+        Integer upp = 3245000;
+        
+        //untuk menampilkan variabel
+        System.out.println("\t"+"\t"+bio+"\n"+"\n"+"Nama : "+nama+"\n"+"Kelas : "+kelas+"\n"+"NPM : "+npm+"\n"+"Program Study : "+ps+"\n"+"TTL : "+ttl+"\n"+"Asal Sekolah : "+as+"\n"+"Awal Kuliah : "+ak+"\n"+"UPP : "+"Rp" +upp);
+    } 
+}
